0e51ab7009b521c5f3981a1985e5c36da84e2535
= <=>
2023-09-02 12:39:28 +0530

    add .env File

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require("dotenv").config()
 
 // User registration
 router.post('/register', async (req, res) => {
     try {
         const { email, password } = req.body;
+
+        // check if user already exist 
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User Already Exists",
+            })
+        }
         const user = new User({ email, password });
         await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Registration failed' });
+
+        return res.status(200).json({
+            success: true,
+            message: "User Created Successfully",
+            data: user
+        });
+    }
+    catch (err) {
+        console.error(err)
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be register,Please try again later",
+        })
     }
 });
 
@@ -19,13 +41,32 @@ router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
+
         if (!user || user.password !== password) {
-            return res.status(401).json({ message: 'Authentication failed' });
+            return res.status(401).json({ message: 'Password is incorrect' });
         }
-        res.json({ user: user, message: 'Logged in successfully' });
+        const payload = {
+            email: user.email,
+            id: user._id
+        };
+
+
+
+        let token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "2h",
+        });
+
+
+        user = user.toObject();
+        user.token = token;
+        user.password = undefined;
+
+
+        res.json({ message: 'Logged in successfully', token });
     } catch (error) {
         res.status(500).json({ message: 'Login failed' });
     }
 });
+
 
 module.exports = router;
